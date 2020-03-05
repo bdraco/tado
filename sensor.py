c2951c8a3425b35d01e307aeb91f1dac532f0e47
J. Nick Koston <nick@koston.org>
2020-03-04 18:31:08 -0600

    more cleanups

--- sensor.py
+++ sensor.py
@@ -1,13 +1,14 @@
 """Support for Tado sensors for each zone."""
 import logging
 
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import TEMP_CELSIUS, UNIT_PERCENTAGE
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity import Entity
 
 from . import DATA, DOMAIN, SIGNAL_TADO_UPDATE_RECEIVED
 from .const import TYPE_AIR_CONDITIONING, TYPE_HEATING, TYPE_HOT_WATER
+from .tado_adapter import TadoZoneData
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -93,6 +94,7 @@ class TadoSensor(Entity):
 
         self._state = None
         self._state_attributes = None
+        self._tado_zone_data = None
 
     async def async_added_to_hass(self):
         """Register for sensor updates."""
@@ -134,9 +136,9 @@ class TadoSensor(Entity):
         if self.zone_variable == "temperature":
             return self.hass.config.units.temperature_unit
         if self.zone_variable == "humidity":
-            return "%"
+            return UNIT_PERCENTAGE
         if self.zone_variable == "heating":
-            return "%"
+            return UNIT_PERCENTAGE
         if self.zone_variable == "ac":
             return ""
 
@@ -160,86 +162,58 @@ class TadoSensor(Entity):
         except KeyError:
             return
 
-        unit = TEMP_CELSIUS
+        self._tado_zone_data = TadoZoneData(data)
 
         if self.zone_variable == "temperature":
-            if "sensorDataPoints" in data:
-                sensor_data = data["sensorDataPoints"]
-                temperature = float(sensor_data["insideTemperature"]["celsius"])
-
-                self._state = self.hass.config.units.temperature(temperature, unit)
-                self._state_attributes = {
-                    "time": sensor_data["insideTemperature"]["timestamp"],
-                    "setting": 0,  # setting is used in climate device
-                }
-
-                # temperature setting will not exist when device is off
-                if (
-                    "temperature" in data["setting"]
-                    and data["setting"]["temperature"] is not None
-                ):
-                    temperature = float(data["setting"]["temperature"]["celsius"])
-
-                    self._state_attributes[
-                        "setting"
-                    ] = self.hass.config.units.temperature(temperature, unit)
+            self._state = self.hass.config.units.temperature(
+                self._tado_zone_data.current_temp, TEMP_CELSIUS
+            )
+            self._state_attributes = {
+                "time": self._tado_zone_data.current_temp_timestamp,
+                "setting": 0,  # setting is used in climate device
+            }
 
         elif self.zone_variable == "humidity":
-            if "sensorDataPoints" in data:
-                sensor_data = data["sensorDataPoints"]
-                self._state = float(sensor_data["humidity"]["percentage"])
-                self._state_attributes = {"time": sensor_data["humidity"]["timestamp"]}
+            self._state = self._tado_zone_data.current_humidity
+            self._state_attributes = {
+                "time": self._tado_zone_data.current_humidity_timestamp
+            }
 
         elif self.zone_variable == "power":
-            if "setting" in data:
-                self._state = data["setting"]["power"]
+            self._state = self._tado_zone_data.power
 
         elif self.zone_variable == "link":
-            if "link" in data:
-                self._state = data["link"]["state"]
+            self._state = self._tado_zone_data.link
 
         elif self.zone_variable == "heating":
-            if "activityDataPoints" in data:
-                activity_data = data["activityDataPoints"]
-
-                if (
-                    "heatingPower" in activity_data
-                    and activity_data["heatingPower"] is not None
-                ):
-                    self._state = float(activity_data["heatingPower"]["percentage"])
-                    self._state_attributes = {
-                        "time": activity_data["heatingPower"]["timestamp"]
-                    }
+            self._state = self._tado_zone_data.heating_power_percentage
+            self._state_attributes = {
+                "time": self._tado_zone_data.heating_power_timestamp
+            }
 
         elif self.zone_variable == "ac":
-            if "activityDataPoints" in data:
-                activity_data = data["activityDataPoints"]
-
-                if "acPower" in activity_data and activity_data["acPower"] is not None:
-                    self._state = activity_data["acPower"]["value"]
-                    self._state_attributes = {
-                        "time": activity_data["acPower"]["timestamp"]
-                    }
+            self._state = self._tado_zone_data.ac_power
+            self._state_attributes = {"time": self._tado_zone_data.ac_power_timestamp}
 
         elif self.zone_variable == "tado bridge status":
-            if "connectionState" in data:
-                self._state = data["connectionState"]["value"]
+            self._state = self._tado_zone_data.connection
 
         elif self.zone_variable == "tado mode":
-            if "tadoMode" in data:
-                self._state = data["tadoMode"]
+            self._state = self._tado_zone_data.tado_mode
 
         elif self.zone_variable == "overlay":
-            self._state = "overlay" in data and data["overlay"] is not None
+            self._state = self._tado_zone_data.overlay_active
             self._state_attributes = (
-                {"termination": data["overlay"]["termination"]["type"]}
+                {"termination": self._tado_zone_data.overlay_termination_type}
                 if self._state
                 else {}
             )
 
         elif self.zone_variable == "early start":
-            self._state = "preparation" in data and data["preparation"] is not None
+            self._state = self._tado_zone_data.preparation is not None
 
         elif self.zone_variable == "open window":
-            self._state = "openWindow" in data and data["openWindow"] is not None
-            self._state_attributes = data["openWindow"] if self._state else {}
+            self._state = self._tado_zone_data.open_window is not None
+            self._state_attributes = (
+                self._tado_zone_data.open_window if self._state else {}
+            )
